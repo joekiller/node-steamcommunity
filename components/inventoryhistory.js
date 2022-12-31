eb37fa2f2610c1e4e595bce9e2ea7d9ec14c8ab5
Joseph Lawson <joe@joekiller.com>
2022-12-30 19:56:29 -0500

    make getInventoryHistory return history events

--- components/inventoryhistory.js
+++ components/inventoryhistory.js
@@ -8,13 +8,10 @@ var Async = require('async');
 /*
  * Inventory history in a nutshell.
  *
- * There are no more page numbers. Now you have to request after_time and optionally after_trade.
- * Without "prev" set, you will request 30 trades that were completed FURTHER IN THE PAST than after_time (and optionally after_trade)
- * With "prev" set, you will request 30 trades that were completed MORE RECENTLY than after_time (and optionally after_trade)
+ * There are no more page numbers. Now you have to request start_time.
  */
 
 /**
- * @deprecated Use GetTradeHistory instead: https://lab.xpaw.me/steam_api_documentation.html#IEconService_GetTradeHistory_v1
  * @param {object} options
  * @param {function} callback
  */
@@ -23,8 +20,6 @@ SteamCommunity.prototype.getInventoryHistory = function(options, callback) {
 		callback = options;
 		options = {};
 	}
-	
-	options.direction = options.direction || "past";
 
 	var qs = "?l=english";
 	if (options.startTime) {
@@ -32,15 +27,7 @@ SteamCommunity.prototype.getInventoryHistory = function(options, callback) {
 			options.startTime = Math.floor(options.startTime.getTime() / 1000);
 		}
 
-		qs += "&after_time=" + options.startTime;
-
-		if (options.startTrade) {
-			qs += "&after_trade=" + options.startTrade;
-		}
-	}
-
-	if (options.direction == "future") {
-		qs += "&prev=1";
+		qs += "&start_time=" + options.startTime;
 	}
 
 	this._myProfile("inventoryhistory" + qs, null, function(err, response, body) {
@@ -58,15 +45,15 @@ SteamCommunity.prototype.getInventoryHistory = function(options, callback) {
 			return;
 		}
 
-		// Load the inventory item data
-		var match2 = body.match(/var g_rgHistoryInventory = (.*);/);
+		// Load the descriptions
+		var match2 = body.match(/var g_rgDescriptions = (.*);/);
 		if (!match2) {
 			callback(new Error("Malformed page: no trade found"));
 			return;
 		}
 
 		try {
-			var historyInventory = JSON.parse(match2[1]);
+			var descriptions = JSON.parse(match2[1]);
 		} catch (ex) {
 			callback(new Error("Malformed page: no well-formed trade data found"));
 			return;
@@ -88,17 +75,20 @@ SteamCommunity.prototype.getInventoryHistory = function(options, callback) {
 			}
 		}
 
-		output.trades = [];
-		var trades = $('.tradehistoryrow');
+		output.events = [];
+		var events = $('.tradehistoryrow');
 
-		var item, trade, profileLink, items, j, econItem, timeMatch, time;
-		for (i = 0; i < trades.length; i++) {
-			item = $(trades[i]);
-			trade = {};
+		var entry, event, profileLink, items, j, timeMatch, time;
+		for (i = 0; i < events.length; i++) {
+			entry = $(events[i]);
+			event = {
+				id: entry.find('[id]')[0].attribs['id'].split('_')[0],
+				type: entry.find('.tradehistory_event_description').text().trim()
+			};
 
-			trade.onHold = !!item.find('span:nth-of-type(2)').text().match(/Trade on Hold/i);
+			event.onHold = !!event.type.match(/was placed on hold/);
 
-			timeMatch = item.find('.tradehistory_timestamp').html().match(/(\d+):(\d+)(am|pm)/);
+			timeMatch = entry.find('.tradehistory_timestamp').html().match(/(\d+):(\d+)(am|pm)/);
 			if (timeMatch[1] == 12 && timeMatch[3] == 'am') {
 				timeMatch[1] = 0;
 			}
@@ -109,36 +99,50 @@ SteamCommunity.prototype.getInventoryHistory = function(options, callback) {
 
 			time = (timeMatch[1] < 10 ? '0' : '') + timeMatch[1] + ':' + timeMatch[2] + ':00';
 
-			trade.date = new Date(item.find('.tradehistory_date').html() + ' ' + time + ' UTC');
-			trade.partnerName = item.find('.tradehistory_event_description a').html();
-			trade.partnerSteamID = null;
-			trade.partnerVanityURL = null;
-			trade.itemsReceived = [];
-			trade.itemsGiven = [];
+			event.date = new Date(entry.find('.tradehistory_date').clone().children().remove().end().text().trim() + ' ' + time + ' UTC');
 
-			profileLink = item.find('.tradehistory_event_description a').attr('href');
-			if (profileLink.indexOf('/profiles/') != -1) {
-				trade.partnerSteamID = new SteamID(profileLink.match(/(\d+)$/)[1]);
-			} else {
-				trade.partnerVanityURL = profileLink.match(/\/([^\/]+)$/)[1];
-				if (options.resolveVanityURLs && vanityURLs.indexOf(trade.partnerVanityURL) == -1) {
-					vanityURLs.push(trade.partnerVanityURL);
-				}
-			}
+			event.partnerName = entry.find('.tradehistory_event_description a').html();
+			event.partnerSteamID = null;
+			event.partnerVanityURL = null;
+			event.plusEvents = [];
+			event.minusEvents = [];
 
-			items = item.find('.history_item');
-			for (j = 0; j < items.length; j++) {
-				match = body.match(new RegExp("HistoryPageCreateItemHover\\( '" + $(items[j]).attr('id') + "', (\\d+), '(\\d+)', '(\\d+|class_\\d+_instance_\\d+|class_\\d+)', '(\\d+)' \\);"));
-				econItem = historyInventory[match[1]][match[2]][match[3]];
-
-				if ($(items[j]).attr('id').indexOf('received') != -1) {
-					trade.itemsReceived.push(new CEconItem(econItem));
+			profileLink = entry.find('.tradehistory_event_description a').attr('href');
+			if(profileLink) {
+				if (profileLink.indexOf('/profiles/') != -1) {
+					event.partnerSteamID = new SteamID(profileLink.match(/(\d+)$/)[1]);
 				} else {
-					trade.itemsGiven.push(new CEconItem(econItem));
+					event.partnerVanityURL = profileLink.match(/\/([^\/]+)$/)[1];
+					if (options.resolveVanityURLs && vanityURLs.indexOf(event.partnerVanityURL) == -1) {
+						vanityURLs.push(event.partnerVanityURL);
+					}
 				}
 			}
 
-			output.trades.push(trade);
+			items = entry.find('.history_item');
+			for (j = 0; j < items.length; j++) {
+				const item = items[j];
+				const appId = item.attribs['data-appid'];
+				const classInstanceId = `${item.attribs['data-classid']}_${item.attribs['data-instanceid']}`;
+				if(appId === '754' && classInstanceId === "0_0") continue;	// this is a comment about a trade hold so skip
+				const description = descriptions[appId][classInstanceId];
+				const contextID = item.attribs['data-contextid'];
+
+				const econItem = {
+					amount: item.attribs['data-amount'],
+					instanceid: item.attribs['data-instanceid']
+				}
+				if(item.attribs['href']) {
+					[,,econItem.id] = item.attribs['href'].match(/(\d+)/g)
+				}
+				if ($(item).parent().parent().children().eq(0).text() === "+") {
+					event.plusEvents.push(new CEconItem(econItem, description, contextID));
+				} else {
+					event.minusEvents.push(new CEconItem(econItem, description, contextID));
+				}
+			}
+
+			output.events.push(event);
 		}
 
 		if (options.resolveVanityURLs) {
@@ -148,15 +152,15 @@ SteamCommunity.prototype.getInventoryHistory = function(options, callback) {
 					return;
 				}
 
-				for (i = 0; i < output.trades.length; i++) {
-					if (output.trades[i].partnerSteamID || !output.trades[i].partnerVanityURL) {
+				for (i = 0; i < output.events.length; i++) {
+					if (output.events[i].partnerSteamID || !output.events[i].partnerVanityURL) {
 						continue;
 					}
 
 					// Find the vanity URL
 					for (j = 0; j < results.length; j++) {
-						if (results[j].vanityURL == output.trades[i].partnerVanityURL) {
-							output.trades[i].partnerSteamID = new SteamID(results[j].steamID);
+						if (results[j].vanityURL == output.events[i].partnerVanityURL) {
+							output.events[i].partnerSteamID = new SteamID(results[j].steamID);
 							break;
 						}
 					}
